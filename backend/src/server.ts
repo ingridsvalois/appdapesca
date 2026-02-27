a58ae37777947470324cc6f8f1edc445426f8f45
ingridvalois <ingridvalois18@outlook.com>
2026-02-27 13:04:55 -0300

    Fix server.ts syntax

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,6 @@ import { env } from "./config/env";
 
 const port = env.port;
 
-app.listen(port, "0.0.0.0" => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Backend App da Pesca rodando na porta ${port}`);
 });
-
