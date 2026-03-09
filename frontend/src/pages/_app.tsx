d7fdaa73b1d3805fae10f7ebed41f1eb24065a0e
ingridvalois <ingridvalois18@outlook.com>
2026-03-09 18:41:52 -0300

    fix: desabilitar SSR para evitar prerender error no build

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import dynamic from "next/dynamic";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
+
+export default dynamic(() => Promise.resolve(App), { ssr: false });
